57baab26ef751d6d48ea0228f8f43e6c5ce02a89
Gabareal <119044507+Gabareal@users.noreply.github.com>
2025-02-27 08:24:12 +0800

    Add files via upload

---
+++ precache-manifest.22f0ccabf27d25d21ee6d35cce251391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c260c8bebce1bff12e9370fd95c7061",
+    "url": "/handposedirectory/index.html"
+  },
+  {
+    "revision": "8f0c4c7782b85748027f",
+    "url": "/handposedirectory/static/css/main.ab58bcba.chunk.css"
+  },
+  {
+    "revision": "b1c2cbeb2ebddbf74946",
+    "url": "/handposedirectory/static/js/2.9435555e.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/handposedirectory/static/js/2.9435555e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0c4c7782b85748027f",
+    "url": "/handposedirectory/static/js/main.d41aa45f.chunk.js"
+  },
+  {
+    "revision": "e732c224501b35ca91fe",
+    "url": "/handposedirectory/static/js/runtime-main.1507a973.js"
+  }
+]);
